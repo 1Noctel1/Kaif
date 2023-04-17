2b46284b1958e3880c96c7f182700f2d241dffdd
1Noctel1 <125264533+1Noctel1@users.noreply.github.com>
2023-04-17 22:18:39 +0300

    Update Большие цифры на 4.py

--- Большие цифры на 4.py
+++ Большие цифры на 4.py
@@ -1,4 +1,16 @@
-num1 = input("Введите первое число: ")
-num2 = input("Введите второе число: ")
-sum = num1 + num2
-print("Результат:" + sum)
+n1 = input("Введите первое число: ")
+n2 = input("Введите второе число: ")
+result = []
+c = 0
+max_len = max(len(n1), len(n2))
+n1 = n1.zfill(max_len)
+n2 = n2.zfill(max_len)
+for i in range(max_len - 1, -1, -1):
+    s = int(n1[i]) + int(n2[i]) + c
+    result.append(str(s % 10))
+    c = s // 10
+    if c > 0:
+        result.append(str(c))
+result.reverse()
+result_str = "".join(result)
+print(f"Результат: {result_str}")
